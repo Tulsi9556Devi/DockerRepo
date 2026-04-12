eabee1e79f1c733d457acd9d90d78c7ff022b92c
TRD <trd.xyz.1234@gmail.com>
2026-04-13 01:23:48 +0530

    Implement HTTP server in app.js
    Added a simple HTTP server that responds with a message.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-FROM node:18
-WORKDIR /app
-COPY . .
-CMD ["node", "app.js"]
+const http = require("http");
+
+http.createServer(function(req, res) {
+    res.write("Hello from Node via Jenkins");
+    res.end();
+}).listen(3000, "0.0.0.0");
